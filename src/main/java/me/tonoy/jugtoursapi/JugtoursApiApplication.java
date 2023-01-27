1540f1a542023c4c1d633bf750edb59664368b81
Md. Tonoy Akando <tonoy.sust@gmail.com>
2023-01-27 21:31:37 +0600

    jugtours

---
+++ src/main/java/me/tonoy/jugtoursapi/JugtoursApiApplication.java
@@ -0,0 +1,12 @@
+package me.tonoy.jugtoursapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugtoursApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JugtoursApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/tonoy/jugtoursapi/JugtoursApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tonoy.jugtoursapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugtoursApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
